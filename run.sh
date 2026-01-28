aeb6cdef5aeae07195a976f344474afd4ebbfea6
Eric Park <me@ericswpark.com>
2026-01-27 22:12:51 -0500

    Fix SSH key type in usage

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ Purpose:    Completely set up a fresh macOS install with specified tools
 
   -h        Show usage (this output) and quit immediately
   -e        Skip essential installs and SSH key generation
-            Only use this flag if your SSH key is not named id_rsa and you
+            Only use this flag if your SSH key is not named id_ed25519 and you
             wish to skip automatic detection of your SSH key. If brew is not
             installed, the rest of the modules will fail.
   -b        Skip brew app installation
